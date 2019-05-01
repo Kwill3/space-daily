c8e699f0db338be7eb3a98fa3644b307c663f904
William Lee <karlweiyang@gmail,com>
2019-05-01 10:15:13 +0100

    Updates

---
+++ precache-manifest.b8ed9ce195450b47ae0b1bd61fbaa496.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2542c1e8c9afe454d5e70408a558a404",
+    "url": "/space-daily/static/media/solar-system.2542c1e8.png"
+  },
+  {
+    "revision": "f2e8ff5a0b2298960c21",
+    "url": "/space-daily/static/js/runtime~main.ed227987.js"
+  },
+  {
+    "revision": "5138d8302d4f8edefc1c",
+    "url": "/space-daily/static/js/main.cdf656dc.chunk.js"
+  },
+  {
+    "revision": "b53f597b6548bf8d656f",
+    "url": "/space-daily/static/js/2.a6753a6c.chunk.js"
+  },
+  {
+    "revision": "5138d8302d4f8edefc1c",
+    "url": "/space-daily/static/css/main.75cf11e3.chunk.css"
+  },
+  {
+    "revision": "e76f8d18126f39002d336c6fd65bc1ef",
+    "url": "/space-daily/index.html"
+  }
+];
